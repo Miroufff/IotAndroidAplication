8715138d0c91ea3ec5ae7d4edb9ec7259eb7c7eb
jerome.auffret <jerome.auffret.ja@gmail.com>
2017-05-17 10:36:29 +0200

    Implement email validation / Use error string

--- app/src/main/java/fr/imie/sensair/activities/RegisterActivity.java
+++ app/src/main/java/fr/imie/sensair/activities/RegisterActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import fr.imie.sensair.R;
 import fr.imie.sensair.model.User;
@@ -33,6 +37,10 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (!isEmailValid(email.getText().toString())) {
+                    Toast.makeText(RegisterActivity.this, getString(R.string.register_not_valid_email), Toast.LENGTH_LONG).show();
+                }
+
                 if (confirm_password.getText().toString().matches(password.getText().toString())) {
                     User currentUser = new User();
                     currentUser
@@ -43,8 +51,32 @@ public class RegisterActivity extends AppCompatActivity {
                         .setLastname("Truc");
 
                     startActivity(new Intent(RegisterActivity.this, SensorActivity.class));
+                } else {
+                    Toast.makeText(RegisterActivity.this, getString(R.string.register_password_not_match), Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+    public boolean isEmailValid(String email)
+    {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
 }
+
