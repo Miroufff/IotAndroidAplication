847346c1f8fc1dfc9f67f8a71dfb47e0c97fa742
Miroufff <sylvain.davenel@laposte.net>
2017-07-17 17:02:11 +0200

    fix fail

--- app/src/main/java/fr/imie/sensair/properties/ApiProperties.java
+++ app/src/main/java/fr/imie/sensair/properties/ApiProperties.java
@@ -7,7 +7,7 @@ package fr.imie.sensair.properties;
 public class ApiProperties {
     private static ApiProperties mInstance= null;
 
-    public String addressServer = "http://192.168.1.89/IotApi/web/app.php";
+    public String addressServer = "http://176.154.157.87/IotApi/web/app.php";
     public String apiPath = "/api";
     public String oauthPath = "/oauth/v2/token";
 
